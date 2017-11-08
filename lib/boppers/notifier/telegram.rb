0dca4fa1bff7655611f7088776237aaad08eb90c
Nando Vieira <fnando.vieira@gmail.com>
2017-11-07 21:58:30 -0800

    Allow setting custom message for Telegram.
    Telegram supports basic text formatting. You can send options to the Telegram notifier like the following:
    
    ```ruby
    Boppers.notify(:message,
                   title: "Some Title",
                   message: "Some message",
                   options: {
                     title: "<b>Some Title</b>",
                     message: "<i>Some message</i>",
                     parse_mode: "HTML"
                   })
    ```

--- lib/boppers/notifier/telegram.rb
+++ lib/boppers/notifier/telegram.rb
@@ -13,12 +13,20 @@ module Boppers
         @subscribe = subscribe
       end
 
-      def call(title, message, _options)
-        bot = TelegramBot.new(token: api_token)
+      def call(title, message, options)
+        telegram_options = options.fetch(:telegram, {})
+        title = telegram_options.delete(:title) { title }
+        message = telegram_options.delete(:message) { message }
 
+        bot = TelegramBot.new(token: api_token)
         notification = TelegramBot::OutMessage.new
         notification.chat = TelegramBot::Channel.new(id: channel_id)
         notification.text = "#{title}\n\n#{message}"
+
+        telegram_options.each do |key, value|
+          notification.public_send("#{key}=", value)
+        end
+
         notification.send_with(bot)
       end
     end
